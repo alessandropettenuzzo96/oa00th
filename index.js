29c27bcca58ef645487e07f8edf9ddddb4ff8d68
Alessandro Pettenuzzo <n00z@MacBook-Pro-di-Alessandro.local>
2018-02-26 15:43:31 +0100

    End. (forse)

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ exports = (clientId, clientSecret, getUserFromid) => {
         authenticate: (scope, authFailedHandler) => {
             return (req, res, next) => {
                 let header = req.headers['Authentication'];
-                if(!header) return authFailedHandler('Authentication header not passed');
+                if(!header) return authFailedHandler({ missingToken: true, message: 'Authentication header not passed' });
                 header = header.split(' ');
-                if(header.length !== 2) return authFailedHandler('Authentication header malformed');
-                if(header[0] !== 'Bearer') return authFailedHandler('Authentication header not Bearer type');
+                if(header.length !== 2) return authFailedHandler({ message: 'Authentication header malformed' });
+                if(header[0] !== 'Bearer') return authFailedHandler({ message: 'Authentication header not Bearer type' });
                 let token = header[1];
-                if(!token) return authFailedHandler('Authentication header not contain token')
+                if(!token) return authFailedHandler({ message: 'Authentication header not contain token' });
                 jwt.verify(token, clientSecret, (err, decoded) => {
-                    if(err || !decoded || !decoded.inn) return authFailedHandler('Invalid AccessToken');
+                    if(err || !decoded || !decoded.inn) return authFailedHandler({ message: 'Invalid AccessToken' });
                     decoded.inn = new Buffer(decoded.inn, 'base64').toString('utf8');
-                    if(!decoded.inn) return authFailedHandler('Invalid AccessToken encoded format.');
+                    if(!decoded.inn) return authFailedHandler({ message: 'Invalid AccessToken encoded format.' });
                     jwt.decode(decoded.inn, (err, inner) => {
-                        if(err || !inner || !inner.usr || !inner.exp || inner.iss || inner.aud || !inner.scp) return authFailedHandler('Token not contain valid informations');
+                        if(err || !inner || !inner.usr || !inner.exp || inner.iss || inner.aud || !inner.scp) return authFailedHandler({ message: 'Token not contain valid informations' });
                         let usr = inner.usr;
                         let exp = inner.exp;
                         let aud = inner.aud;
                         let scp = inner.scp.split(' ');
                         let iss = inner.iss;
-                        if((new Date()).getTime() >= exp) return authFailedHandler('Token is expired');
-                        if(aud !== clientId) return authFailedHandler('Token has invalid audience');
-                        if(iss !== 'n00z_oauth_server') return authFailedHandler('Token has invalid issuer');
-                        if(scp.filter((el) => { scope.indexOf(el) }).length !== scope.length) return authFailedHandler('User not has sufficient privileges');
+                        if((new Date()).getTime() >= exp) return authFailedHandler({ isExpired: true, message: 'Token is expired' });
+                        if(aud !== clientId) return authFailedHandler({ message: 'Token has invalid audience' });
+                        if(iss !== 'n00z_oauth_server') return authFailedHandler({ message: 'Token has invalid issuer' });
+                        if(scp.filter((el) => { scope.indexOf(el) }).length !== scope.length) return authFailedHandler({ insufficientScopes: true, message: 'User not has sufficient privileges'});
                         getUserFromid(usr).then((user) => {
                             req.user = user;
                             next();
@@ -45,7 +45,7 @@ exports = (clientId, clientSecret, getUserFromid) => {
                     if(!response) return reject('Unexpected response');
                     return resolve(response);
                 }).catch((err) => {
-                    return resolve(err.message);
+                    return reject(err.message);
                 });
             });
         },
@@ -56,14 +56,19 @@ exports = (clientId, clientSecret, getUserFromid) => {
                     if(!response) return reject('Unexpected response');
                     return resolve(response);
                 }).catch((err) => {
-                    return resolve(err.message);
+                    return reject(err.message);
                 })
             });
         },
 
-        logout: (accessToken) => {
+        logout: (refreshToken) => {
             return new Promise((resolve, reject) => {
-
+                axios.post(BASE_API_PATH+'oauth/revoke', { refresh_token: refreshToken }).then((response) => {
+                    if(!response || response.status !== 'revoked' ) return reject('Unexpected response');
+                    resolve();
+                }).catch((err) => {
+                    return reject(err.message);
+                });
             });
         },
 
@@ -73,6 +78,17 @@ exports = (clientId, clientSecret, getUserFromid) => {
                 axios.post(BASE_API_PATH+"app/"+clientId+"/users/create", { client_id: clientId, client_secret: clientSecret, username: username, password: password, scope: scopes.join(' ')}).then((user) => {
                     if(!user) return reject('Unexpected response');
                     return resolve(user);
+                }).catch((err) => {
+                    return reject(err.message);
+                });
+            });
+        },
+
+        deleteUser: (userId) => {
+            return new Promise((resolve, reject) => {
+                axios.delete(BASE_API_PATH+'app/'+clientId+'/users/'+userId).then((response) => {
+                    if( !response || response.status !== 'revoke' ) return reject('Unexpected response');
+                    return resolve();
                 }).catch((err) => {
                     return reject(err.message);
                 });
