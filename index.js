543960ba8eddebd103b103ec0ca01e72723caafd
Alessandro Pettenuzzo <n00z@MBPdiAlessandro.station>
2018-03-03 02:13:02 +0100

    Return in ES5.1

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports.Oa00th = function (clientId, clientSecret, getUserFromId) {
             if (header[0] !== 'Bearer') return authFailedHandler(req, res, next, {message: 'Authentication header not Bearer type'});
             let token = header[1];
             if (!token) return authFailedHandler(req, res, next, {message: 'Authentication header not contain token'});
-            jwt.verify(token, that.clientSecret, (err, decoded) => {
+            jwt.verify(token, clientSecret, (err, decoded) => {
                 console.log(JSON.stringify(err));
                 console.log(JSON.stringify(decoded));
                 if (err || !decoded || !decoded.inn) return authFailedHandler(req, res, next, {message: 'Invalid AccessToken'});
@@ -33,7 +33,7 @@ module.exports.Oa00th = function (clientId, clientSecret, getUserFromId) {
                         isExpired: true,
                         message: 'Token is expired'
                     });
-                    if (aud !== that.clientId) return authFailedHandler(req, res, next, {message: 'Token has invalid audience'});
+                    if (aud !== clientId) return authFailedHandler(req, res, next, {message: 'Token has invalid audience'});
                     if (iss !== 'n00z_oauth_server') return authFailedHandler(req, res, next, {message: 'Token has invalid issuer'});
                     if (scp.filter((el) => {
                             scope.indexOf(el)
@@ -41,7 +41,7 @@ module.exports.Oa00th = function (clientId, clientSecret, getUserFromId) {
                         insufficientScopes: true,
                         message: 'User not has sufficient privileges'
                     });
-                    that.getUserFromId(usr).then((user) => {
+                    getUserFromId(usr).then((user) => {
                         req.user = user;
                         next();
                     }).catch((err) => {
@@ -55,9 +55,9 @@ module.exports.Oa00th = function (clientId, clientSecret, getUserFromId) {
     this.login = function(username, password, scopes) {
         let that = this;
         return new Promise((resolve, reject) => {
-            axios.post(that.BASE_API_PATH + 'oauth/token', {
-                client_id: that.clientId,
-                client_secret: that.clientSecret,
+            axios.post(BASE_API_PATH + 'oauth/token', {
+                client_id: clientId,
+                client_secret: clientSecret,
                 username: username,
                 password: password,
                 grant_type: 'password',
@@ -74,9 +74,9 @@ module.exports.Oa00th = function (clientId, clientSecret, getUserFromId) {
     this.refresh = function(refreshToken) {
         let that = this;
         return new Promise((resolve, reject) => {
-            axios.post(that.BASE_API_PATH + 'oauth/token', {
-                client_id: that.clientId,
-                client_secret: that.clientSecret,
+            axios.post(BASE_API_PATH + 'oauth/token', {
+                client_id: clientId,
+                client_secret: clientSecret,
                 grant_type: 'refresh_token',
                 refresh_token: refreshToken
             }).then((response) => {
@@ -90,7 +90,7 @@ module.exports.Oa00th = function (clientId, clientSecret, getUserFromId) {
 
     this.logout = function(refreshToken) {
         return new Promise((resolve, reject) => {
-            axios.post(that.BASE_API_PATH + 'oauth/revoke', {refresh_token: refreshToken}).then((response) => {
+            axios.post(BASE_API_PATH + 'oauth/revoke', {refresh_token: refreshToken}).then((response) => {
                 if (!response || response.status !== 'revoked') return reject('Unexpected response');
                 resolve();
             }).catch((err) => {
@@ -103,9 +103,9 @@ module.exports.Oa00th = function (clientId, clientSecret, getUserFromId) {
         let that = this;
         return new Promise((resolve, reject) => {
             if (!scopes) return reject('Scopes not defined');
-            axios.post(that.BASE_API_PATH + "app/" + that.clientId + "/users/create", {
-                client_id: that.clientId,
-                client_secret: that.clientSecret,
+            axios.post(BASE_API_PATH + "app/" + clientId + "/users/create", {
+                client_id: clientId,
+                client_secret: clientSecret,
                 username: username,
                 password: password,
                 scope: scopes.join(' ')
@@ -121,7 +121,7 @@ module.exports.Oa00th = function (clientId, clientSecret, getUserFromId) {
     this.deleteUser = function(userId) {
         let that = this;
         return new Promise((resolve, reject) => {
-            axios.delete(that.BASE_API_PATH + 'app/' + that.clientId + '/users/' + userId).then((response) => {
+            axios.delete(BASE_API_PATH + 'app/' + clientId + '/users/' + userId).then((response) => {
                 if (!response || response.status !== 'deleted') return reject('Unexpected response');
                 return resolve();
             }).catch((err) => {
